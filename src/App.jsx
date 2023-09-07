fa9eb8b3a03692caaabb2a56198c6711c1a7e8d0
Romain Laurent <romain.laurent@cdiscount.com>
2023-09-07 15:59:42 +0200

    feat: 🎸 add Counter

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,11 @@
+import Counter from "./Counter";
+
 export default function App() {
-  return "Sales";
+  return (
+    <>
+      <Counter />
+      <Counter initialValue={20} step={2} />
+      <Counter initialValue={-13} />
+    </>
+  );
 }
