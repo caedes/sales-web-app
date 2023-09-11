0ac36e24c1df96772eba104676ce46a6516a0f49
Romain Laurent <romain.laurent@cdiscount.com>
2023-09-11 14:26:35 +0200

    feat: 🎸 add CssBaseline

--- src/App/App.jsx
+++ src/App/App.jsx
@@ -1,5 +1,6 @@
 import { RouterProvider } from "react-router-dom";
 import { ThemeProvider } from "@mui/material/styles";
+import { CssBaseline } from "@mui/material";
 
 import { router } from "./router";
 import { theme } from "./theme";
@@ -7,6 +8,7 @@ import { theme } from "./theme";
 export default function App() {
   return (
     <ThemeProvider theme={theme}>
+      <CssBaseline />
       <RouterProvider router={router} />
     </ThemeProvider>
   );
