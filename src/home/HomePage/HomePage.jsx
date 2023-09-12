9a80b72be3adfe24ce52f9dd1dd5c031ec88cef3
Romain Laurent <romain.laurent@cdiscount.com>
2023-09-12 16:46:05 +0200

    feat: 🎸 filter over Product#condition on HomePage

--- src/home/HomePage/HomePage.jsx
+++ src/home/HomePage/HomePage.jsx
@@ -1,4 +1,13 @@
-import { Box, LinearProgress } from "@mui/material";
+import {
+  FormControl,
+  FormControlLabel,
+  FormLabel,
+  LinearProgress,
+  Radio,
+  RadioGroup,
+  Stack,
+  TextField,
+} from "@mui/material";
 import { DataGrid } from "@mui/x-data-grid";
 
 import { ProductCondition } from "../ProductCondition";
@@ -6,52 +15,92 @@ import { ProductDescription } from "../ProductDescription";
 import { ProductImage } from "../ProductImage";
 import { ProductPrice } from "../ProductPrice";
 import { withRow } from "../../hoc";
-import { useProducts } from "../../hooks";
+import { useCondition, useProducts } from "../../hooks";
+
+const conditionTypes = [
+  {
+    label: "Toutes",
+    value: "",
+  },
+  {
+    label: "Neuf",
+    value: "new",
+  },
+  {
+    label: "Occasion",
+    value: "used",
+  },
+];
+
+const productColumns = [
+  {
+    field: "imageUrl",
+    headerName: "Image produit",
+    renderCell: withRow(ProductImage),
+  },
+  {
+    field: "description",
+    headerName: "Description",
+    flex: 1,
+    renderCell: withRow(ProductDescription),
+  },
+  {
+    field: "price",
+    headerName: "Prix",
+    renderCell: withRow(ProductPrice),
+  },
+  {
+    field: "condition",
+    headerName: "État",
+    renderCell: withRow(ProductCondition),
+  },
+  { field: "stock", headerName: "Stock" },
+];
 
 export default function HomePage() {
-  const { isLoading, error, data: products } = useProducts();
-
-  if (isLoading) return <LinearProgress />;
+  const [condition, setCondition] = useCondition();
+  const { isLoading, error, data: products } = useProducts({ condition });
 
   // TODO Est-ce qu'on pourrait pas utiliser un message d'erreur
   // pour l'utilisateur à la hauteur de l'app entière ?
   // https://github.com/bvaughn/react-error-boundary#readme
   // https://mui.com/material-ui/react-snackbar/#customization
-  if (error) return "oulala";
-
-  const productColumns = [
-    {
-      field: "imageUrl",
-      headerName: "Image produit",
-      renderCell: withRow(ProductImage),
-    },
-    {
-      field: "description",
-      headerName: "Description",
-      flex: 1,
-      renderCell: withRow(ProductDescription),
-    },
-    {
-      field: "price",
-      headerName: "Prix",
-      renderCell: withRow(ProductPrice),
-    },
-    {
-      field: "condition",
-      headerName: "État",
-      renderCell: withRow(ProductCondition),
-    },
-    { field: "stock", headerName: "Stock" },
-  ];
+  if (error) throw "oulala";
 
   return (
-    <Box sx={{ width: "100%" }}>
-      <DataGrid
-        rows={products}
-        columns={productColumns}
-        disableRowSelectionOnClick
-        autoHeight
-      />
-    </Box>
+    <>
+      <Stack direction="row" justifyContent="center" spacing={6} sx={{ my: 4 }}>
+        <TextField label="Recherche" sx={{ width: 400 }} />
+        <FormControl>
+          <FormLabel id="condition-label">Condition</FormLabel>
+          <RadioGroup
+            row
+            aria-labelledby="condition-label"
+            name="condition"
+            value={condition}
+            onChange={setCondition}
+          >
+            {conditionTypes.map(({ label, value }) => (
+              <FormControlLabel
+                key={label}
+                value={value}
+                control={<Radio />}
+                label={label}
+              />
+            ))}
+          </RadioGroup>
+        </FormControl>
+      </Stack>
+      {isLoading ? (
+        <LinearProgress />
+      ) : (
+        <DataGrid
+          rows={products}
+          columns={productColumns}
+          disableRowSelectionOnClick
+          autoHeight
+        />
+      )}
+    </>
   );
 }
--- src/hooks/useProducts.js
+++ src/hooks/useProducts.js
@@ -2,9 +2,15 @@ import { useQuery } from "@tanstack/react-query";
 
 const baseUrl = import.meta.env.VITE_API_BASE_URL || "http://localhost:3001";
 
-export default function useProducts() {
-  return useQuery(["products"], () =>
-    // TODO Client HTTP ? Axios ? Ky ?
-    fetch(`${baseUrl}/products`).then((response) => response.json()),
+export default function useProducts({ condition }) {
+  const searchParams = new URLSearchParams();
+
+  if (condition) searchParams.set("condition", condition);
+
+  return useQuery(["products", { condition }], () =>
+    // TODO HTTP Client ? Axios ? Ky ?
+    fetch(`${baseUrl}/products?${searchParams}`).then((response) =>
+      response.json(),
+    ),
   );
 }
