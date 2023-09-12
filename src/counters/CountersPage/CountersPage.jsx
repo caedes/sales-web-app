c45eb24ae995321202a2d6594464265fdedef3a7
Romain Laurent <romain.laurent@cdiscount.com>
2023-09-12 11:07:32 +0200

    refactor: 💡 move Counter into /ds/organisms

--- src/counters/CountersPage/CountersPage.jsx
+++ src/counters/CountersPage/CountersPage.jsx
@@ -1,6 +1,6 @@
 import { Typography } from "@mui/material";
 
-import { Counter } from "../Counter";
+import { Counter } from "../../ds/organisms";
 
 export default function CountersPage() {
   return (
